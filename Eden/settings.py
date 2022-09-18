976d7a8e544cc91378af2b0dfcd8cfe097b4d93e
afv22 <38698043+afv22@users.noreply.github.com>
2022-09-18 12:10:08 -0400

    Allow all CORS origins

--- Eden/settings.py
+++ Eden/settings.py
@@ -39,7 +39,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapp.com", "localhost"]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+# CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+CORS_ALLOW_ALL_ORIGINS = True
 
 # CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
 
