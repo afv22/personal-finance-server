b1dffaf7371bc0376c9c0449d77871971ef953d3
afv22 <38698043+afv22@users.noreply.github.com>
2022-09-19 20:59:48 -0400

    Removeing grpahql auth because it was breaking the connection to the app

--- Eden/urls.py
+++ Eden/urls.py
@@ -1,14 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # Remove csrf_exempt for production. Adding it now because it blocks the app every time
-    path(
-        "graphql/",
-        csrf_exempt(views.PrivateGraphQLView.as_view(graphiql=True)),
-    ),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
     path("", views.index),
 ]
--- Eden/views.py
+++ Eden/views.py
@@ -1,11 +1,5 @@
 from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
-from graphene_django.views import GraphQLView
 
 
 def index(request):
     return HttpResponse("Welcome to Eden!")
-
-
-class PrivateGraphQLView(LoginRequiredMixin, GraphQLView):
-    pass
