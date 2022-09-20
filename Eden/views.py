84f3bc3ca9f39200053e84e0fa2174f3765af896
afv22 <38698043+afv22@users.noreply.github.com>
2022-09-19 20:54:01 -0400

    Require authentication for graphql UI

--- Eden/views.py
+++ Eden/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from graphene_django.views import GraphQLView
+
 
 def index(request):
-    return HttpResponse('Welcome to Eden!')
+    return HttpResponse("Welcome to Eden!")
+
+
+class PrivateGraphQLView(LoginRequiredMixin, GraphQLView):
+    pass
